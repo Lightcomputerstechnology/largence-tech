bab85ae7ce39a22e781620df9ddfc114f7daa340
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-09-10 04:22:32 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,18 @@ export default {
   theme: {
     extend: {
       colors: {
-        brand: { DEFAULT:"#E11900", hover:"#C31500", ring:"#FDE8E5", 50:"#FFF1EF", 600:"#C31500", 700:"#A30F00" }
+        brand: {
+          DEFAULT: "#E11900",
+          hover: "#C31500",
+          ring: "#FDE8E5",
+          50: "#FFF1EF",
+          600: "#C31500",
+          700: "#A30F00",
+        },
       },
-      borderRadius: { xl: "12px" }
+      borderRadius: {
+        xl: "12px",
+      },
     },
   },
   plugins: [],
